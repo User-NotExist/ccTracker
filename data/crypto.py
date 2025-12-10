ba606e4ffe99cbb756328f33ebbf3608a13d6baf
User-NotExist <49120419+User-NotExist@users.noreply.github.com>
2025-12-10 14:58:15 +0700

    Created Crypto Data

---
+++ data/crypto.py
@@ -0,0 +1,29 @@
+from utils.event import Event
+from utils.binance_websocket import BinanceWebSocket
+
+class Crypto:
+    def __init__(self, symbol: str):
+        """Initialize the Crypto instance, setting the trading pair symbol."""
+        self.symbol = symbol
+
+        self.ws_ticker = BinanceWebSocket(symbol, 'ticker')
+        self.ws_trade = BinanceWebSocket(symbol, 'trade')
+        self.ws_kline_1m = BinanceWebSocket(symbol, 'kline_1m')
+        self.ws_depth = BinanceWebSocket(symbol, 'depth')
+        self.ws_aggregate_trade = BinanceWebSocket(symbol, 'aggTrade')
+
+    def connect_all(self):
+        """Establish WebSocket connections for all data streams."""
+        self.ws_ticker.connect()
+        self.ws_trade.connect()
+        self.ws_kline_1m.connect()
+        self.ws_depth.connect()
+        self.ws_aggregate_trade.connect()
+
+    def close_all(self):
+        """Close all WebSocket connections."""
+        self.ws_ticker.close()
+        self.ws_trade.close()
+        self.ws_kline_1m.close()
+        self.ws_depth.close()
+        self.ws_aggregate_trade.close()
